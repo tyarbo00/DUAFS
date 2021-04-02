10585ec102f16f1a58e0dcaf6f562d09ae77b015
Tyce <78239799+tyarbo00@users.noreply.github.com>
2021-04-01 23:49:02 -0500

    Update DUAFS DATA SQL.sql

--- DUAFS DATA SQL.sql
+++ DUAFS DATA SQL.sql
@@ -2,20 +2,81 @@
 	DUAFS Test Data MySQL File
 */
 
-USE DUAFS;
+USE duafs;
 
+/*ALL users passwords are 'test' below */
 INSERT INTO user
 VALUES
-	(default,'jharde00','testpassword','jharde00@g.uafs.edu','James','D','Harden',1,0,0,0),
-    (default,'wsmith01','testpassword','wsmith01@g.uafs.edu','Will','','Smith',1,0,0,0),
-    (default,'djones00','testpassword','david.jones@uafs.edu','David','J','Jones',0,1,1,0),
-    (default,'mharri00','testpassword','mike.harris@uafs.edu','Mike','S','Harris',0,1,0,0),
-    (default,'kscott00','testpassword','kim.scott@uafs.edu','Kim','','Scott',0,0,1,0),
-    (default,'gjames00','testpassword','admin@uafs.edu','Gray','M','James',0,0,0,1);
+	/*Student*/
+	(default,'jbutle01','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','jbutler01@g.uafs.edu','Jimmy','','Butler',1,0,0,0),
+	(default,'mmille00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','mmille00@g.uafs.edu','Mike','','Miller',1,0,0,0),
+	(default,'kann00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy',  'kann00@g.uafs.edu','Kayla','L','Ann',1,0,0,0),
+	(default,'jharde00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','jharde00@g.uafs.edu','James','D','Harden',1,0,0,0),
+	(default,'sscott01','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','sscott01@g.uafs.edu','Sarah','M','Scott',1,0,0,0),
+    	(default,'wsmith01','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','wsmith01@g.uafs.edu','Will','','Smith',1,0,0,0),
+	(default,'tdunca00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','tdunca00@g.uafs.edu','Tim','','Duncan',1,0,0,0),
+	(default,'rfoste00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','rfoste00@g.uafs.edu','Renee','S','Foster',1,0,0,0),
+	(default,'jstric01','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','jstric01@g.uafs.edu','Janet','J','Strickland',1,0,0,0),
+	(default,'mcolli00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','mcolli00@g.uafs.edu','Michael','S','Collins',1,0,0,0),
+	(default,'rstilt00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','rstilt00@g.uafs.edu','Robert','R','Stiltner',1,0,0,0),
+	(default,'gmulli00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','gmulli00@g.uafs.edu','Galye','L','Mulligan',1,0,0,0),
+	(default,'cjaspe00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','cjaspe00@g.uafs.edu','Charles','M','Jasper',1,0,0,0),
+	(default,'nrobbi00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','nrobbi00@g.uafs.edu','Norman','P','Robbins',1,0,0,0),
+	(default,'lburke00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','lburke@g.uafs.edu','Lauren','H','Burke',1,0,0,0),
+	(default,'awelch00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','awelch00@g.uafs.edu','Amy','R','Welch',1,0,0,0),
+	(default,'mmatth00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','mmatth00@g.uafs.edu','Michael','H','Matthew',1,0,0,0),
+	(default,'jmoren00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','jmoren00@g.uafs.edu','James','S','Moreno',1,0,0,0),
+	(default,'ablea00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','ablea00@g.uafs.edu','Andrew','C','Blea',1,0,0,0),
+	(default,'bbryan00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','bbryan00@g.uafs.edu','Brenda','G','Bryant',1,0,0,0),
+	(default,'jwiggi00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','jwiggi00@g.uafs.edu','Jackie','L','Wiggins',1,0,0,0),
+	(default,'lcourt00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','lcourt00@g.uafs.edu','Lawrence','D','Courtney',1,0,0,0),
+	(default,'mhall00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','mhall00@g.uafs.edu','Mattie','S','Hall',1,0,0,0),
+	(default,'rgleas00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','rgleas00@g.uafs.edu','Rosalie','N','Gleason',1,0,0,0),
+	(default,'jconra00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','jconra00@g.uafs.edu','James','A','Conrad',1,0,0,0),
+	(default,'cmatte00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','cmatte00@g.uafs.edu','Chris','P','Matteo',1,0,0,0),
+	/*Professor      27*/
+  	(default,'mharri00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','mike.harris@uafs.edu','Mike','S','Harris',0,1,0,0),
+	(default,'bheneh00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','brian.henehan@uafs.edu','Brian','','Henehan',0,1,0,0),
+	(default,'jhight00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','john.hightower@uafs.edu','John','','Hightower',0,1,0,0),
+	(default,'gfulen00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','gail.fulenwider@uafs.edu','Gail','','Fulenwider',0,1,0,0),
+	(default,'hhardi00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','hong.harding@uafs.edu','Hong','B','Harding',0,1,0,0),
+	(default,'json00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','john.son@uafs.edu','John','D','Son',0,1,0,0),
+	(default,'jsanro00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','jean.sanroman@uafs.edu','Jean','D','Sanroman',0,1,0,0),
+	(default,'jhasto00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','jennifer.haston@uafs.edu','Jennifer','J','Haston',0,1,0,0),
+	(default,'sdarda00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','samantha.dardar@uafs.edu','Samantha','J','Dardar',0,1,0,0),
+	(default,'trodge00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','terry.rodgers@uafs.edu','Terry','','Rodgers',0,1,0,0),
+	(default,'ldunca00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','larry.duncan@uafs.edu','Larry','D','Duncan',0,1,0,0),
+	(default,'jhasto00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','jennifer.haston@uafs.edu','Jennifer','J','Haston',0,1,0,0),
+	(default,'mchase00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','miriam.chase@uafs.edu','Miriam','B','Chase',0,1,0,0),
+	(default,'rrobbi00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','ronald.robbins@uafs.edu','Ronald','M','Robbins',0,1,0,0),
+	(default,'brobbi00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','barbara.robbins@uafs.edu','Barbara','F','Robbins',0,1,0,0),
+	(default,'bgorma00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','bradley.gorman@uafs.edu','Bradley','','Gorman',0,1,0,0),
+	(default,'dhong00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','donald.hong@uafs.edu','Donald','','Hong',0,1,0,0),
+	(default,'avalen00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','alfred.valenzuela@uafs.edu','Alfred','C','Valenzuela',0,1,0,0),
+	(default,'ekirk00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','elizabeth.kirk@uafs.edu','Elizabeth','A','Kirk',0,1,0,0),
+	(default,'amaddu00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','angie.maddux@uafs.edu','Angie','M','Maddux',0,1,0,0),
+	(default,'bochoa00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','bill.ochoa@uafs.edu','Bill','T','Ochoa',0,1,0,0),
+	/*Professor/Advisor   48*/
+	(default,'djones00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','david.jones@uafs.edu','David','J','Jones',0,1,1,0),
+	(default,'bmclau00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','brian.mclaughlan@uafs.edu','Brian','','Mclaughlan',0,1,1,0),
+	(default,'dcharp00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','dale.charpentier@uafs.edu','Dale','M','Charpentier',0,1,1,0),
+	(default,'dcarde00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','dora.carden@uafs.edu','Dora','C','Carden',0,1,1,0),
+	(default,'ldonat00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','laura.donato@uafs.edu','Laura','','Donato',0,1,1,0),
+	(default,'dlyle00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','debra.lyle@uafs.edu','Debra','J','Lyle',0,1,1,0),
+	/*Advisor    54*/
+    	(default,'kscott00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','kim.scott@uafs.edu','Kim','','Scott',0,0,1,0),
+	(default,'jfrits00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','janet.fritsche@uafs.edu','Janet','G','Fritsche',0,0,1,0),
+	(default,'tscurl00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','terry.scurlock@uafs.edu','Terry','S','Scurlock',0,0,1,0),
+	(default,'smered00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','sandra.meredith@uafs.edu','Sandra','S','Meredith',0,0,1,0),
+	(default,'jkapla00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','judy.kaplan@uafs.edu','Judy','M','Kaplan',0,0,1,0),
+	/*Administrator  59*/
+	(default,'kbuckw00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','karen.admin@uafs.edu','Karen','J','Buckwater',0,0,0,1),
+	(default,'msmith00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','mary.admin@uafs.edu','Mary','','Smtih',0,0,0,1),
+    	(default,'gjames00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','gray.admin@uafs.edu','Gray','M','James',0,0,0,1);
     
 INSERT INTO college
 VALUES
-	(default,'Applied Science and Technology'),
+    (default,'Applied Science and Technology'),
     (default,'Business'),
     (default,'Communication, Languages, Arts, and Social Sciences'),
     (default,'Graduate Studies'),
@@ -25,15 +86,15 @@ VALUES
     
 INSERT INTO department
 VALUES
-	(default,'Applied Science and Technology',1),
-	(default,'Business',2),
+    (default,'Applied Science and Technology',1),
+    (default,'Business',2),
     (default,'Art',3),
-	(default,'Music',3),
+    (default,'Music',3),
     (default,'History',3),
     (default,'Dental Hygiene',5),
     (default,'Nursing',5),
     (default,'Biological Sciences',6),
-	(default,'Computer and Information Sciences',6),
+    (default,'Computer and Information Sciences',6),
     (default,'Engineering',6),
     (default,'Mathematics',6),
     (default,'Physical Sciences',6),
@@ -42,7 +103,7 @@ VALUES
     
 INSERT INTO program 
 VALUES
-	(default,'Animation Technology',TRUE,FALSE,1),
+    (default,'Animation Technology',TRUE,FALSE,1),
     (default,'Applied Science',TRUE,FALSE,1),
     (default,'Automotive Technology',FALSE,TRUE,1),
     (default,'Accounting',TRUE,FALSE,2),
@@ -53,16 +114,17 @@ VALUES
     (default,'Dental Hygiene',TRUE,FALSE,5),
     (default,'Nursing',TRUE,FALSE,5),
     (default,'Radiography',FALSE,TRUE,5),
-	(default,'Information Technology',TRUE,FALSE,6),
-	(default,'Biology',TRUE,FALSE,6),
-	(default,'Mathematics',TRUE,FALSE,6),
-	(default,'Engineering',TRUE,FALSE,6),
-	(default,'Elementary Education K-6',TRUE,FALSE,7),
+    (default,'Information Technology',TRUE,FALSE,6),
+    (default,'Biology',TRUE,FALSE,6),
+    (default,'Mathematics',TRUE,FALSE,6),
+    (default,'Engineering',TRUE,FALSE,6),
+    (default,'Elementary Education K-6',TRUE,FALSE,7),
     (default,'Middle Childhood Education 4-8',TRUE,FALSE,7);
     
 INSERT INTO major
 VALUES
-	(4064,'Animation Technology',1),
+    (4000,'Information Technology',12),
+    (4063,'Animation Technology',1),
     (4100,'Applied Science',2),
     (0578,'Business Administration',4),
     (2545,'English',6),
@@ -85,14 +147,89 @@ VALUES
    
 INSERT INTO minor
 VALUES
-	(1001,'Biology',13),
+    (1001,'Biology',13),
     (1030,'Applied Statistics',14),
     (4030,'General Technology - Automotive',3);
     
 INSERT INTO advisor 
 VALUES
-	(default,3,2),
-	(default,5,5);
+	/*Professor/Advisor   6*/
+	(default,48,1),
+	(default,49,6),
+	(default,50,2),
+	(default,51,3),
+	(default,52,4),
+	(default,53,5),
+	/*Just Advisor*/
+	(default,54,6),
+	(default,55,7),
+	(default,56,2),
+	(default,57,5),
+	(default,58,7);
     
+INSERT INTO professor
+VALUES
+	/*Just Professor*/
+	(default,27,1),
+	(default,28,2),
+	(default,29,3),
+	(default,30,4),
+	(default,31,5),
+	(default,32,6),
+	(default,33,7),
+	(default,34,8),
+	(default,35,9),
+	(default,36,10),
+	(default,37,11),
+	(default,38,12),
+	(default,39,13),
+	(default,40,14),
+	(default,41,9),
+	(default,42,10),
+	(default,43,11),
+	(default,44,12),
+	(default,45,13),
+	(default,46,14),
+	(default,47,1),
+	/*Professor/Advisor*/
+	(default,48,1),
+	(default,49,9),
+	(default,50,2),
+	(default,51,3),
+	(default,52,2),
+	(default,53,7);
+	
+INSERT INTO student
+VALUES 	
+	(default,1,TRUE,4063,NULL,1),
+	(default,2,TRUE,4100,NULL,1),
+	(default,3,TRUE,0578,NULL,3),
+	(default,4,TRUE,2545,NULL,4),
+	(default,5,TRUE,3576,NULL,4),
+	(default,6,TRUE,2565,NULL,4),
+	(default,7,TRUE,3574,NULL,4),
+	(default,8,TRUE,0005,NULL,8),
+	(default,9,TRUE,2062,NULL,6),
+	(default,10,TRUE,2018,1001,10),
+	(default,11,TRUE,2040,NULL,10),
+	(default,12,TRUE,1054,NULL,2),
+	(default,13,TRUE,3006,NULL,9),
+	(default,14,TRUE,3565,NULL,9),
+	(default,15,TRUE,3063,NULL,2),
+	(default,16,TRUE,3568,NULL,2),
+	(default,17,TRUE,0020,NULL,7),
+	(default,18,TRUE,0021,1001,7),
+	(default,19,TRUE,3554,NULL,8),
+	(default,20,TRUE,3554,NULL,8),
+	(default,21,TRUE,3515,NULL,11),
+	(default,22,TRUE,4000,1030,2),
+	(default,23,TRUE,4000,NULL,7),
+	(default,24,TRUE,1054,1030,2),
+	(default,25,FALSE,0578,NULL,9),
+	(default,26,FALSE,3063,NULL,9);
     
-    
+	
+	
+	
+	
+	
