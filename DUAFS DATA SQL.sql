912e089d802a73cf7c0074d73a2b71f6e49d9774
Tyce <78239799+tyarbo00@users.noreply.github.com>
2021-04-11 13:25:07 -0500

    Update DUAFS DATA SQL.sql

--- DUAFS DATA SQL.sql
+++ DUAFS DATA SQL.sql
@@ -13,7 +13,7 @@ VALUES
 	(default,'kann00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy',  'kann00@g.uafs.edu','Kayla','L','Ann',1,0,0,0),
 	(default,'jharde00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','jharde00@g.uafs.edu','James','D','Harden',1,0,0,0),
 	(default,'sscott01','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','sscott01@g.uafs.edu','Sarah','M','Scott',1,0,0,0),
-    	(default,'wsmith01','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','wsmith01@g.uafs.edu','Will','','Smith',1,0,0,0),
+        (default,'wsmith01','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','wsmith01@g.uafs.edu','Will','','Smith',1,0,0,0),
 	(default,'tdunca00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','tdunca00@g.uafs.edu','Tim','','Duncan',1,0,0,0),
 	(default,'rfoste00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','rfoste00@g.uafs.edu','Renee','S','Foster',1,0,0,0),
 	(default,'jstric01','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','jstric01@g.uafs.edu','Janet','J','Strickland',1,0,0,0),
@@ -35,7 +35,7 @@ VALUES
 	(default,'jconra00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','jconra00@g.uafs.edu','James','A','Conrad',1,0,0,0),
 	(default,'cmatte00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','cmatte00@g.uafs.edu','Chris','P','Matteo',1,0,0,0),
 	/*Professor      27*/
-  	(default,'mharri00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','mike.harris@uafs.edu','Mike','S','Harris',0,1,0,0),
+        (default,'mharri00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','mike.harris@uafs.edu','Mike','S','Harris',0,1,0,0),
 	(default,'bheneh00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','brian.henehan@uafs.edu','Brian','','Henehan',0,1,0,0),
 	(default,'jhight00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','john.hightower@uafs.edu','John','','Hightower',0,1,0,0),
 	(default,'gfulen00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','gail.fulenwider@uafs.edu','Gail','','Fulenwider',0,1,0,0),
@@ -64,7 +64,7 @@ VALUES
 	(default,'ldonat00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','laura.donato@uafs.edu','Laura','','Donato',0,1,1,0),
 	(default,'dlyle00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','debra.lyle@uafs.edu','Debra','J','Lyle',0,1,1,0),
 	/*Advisor    54*/
-    	(default,'kscott00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','kim.scott@uafs.edu','Kim','','Scott',0,0,1,0),
+        (default,'kscott00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','kim.scott@uafs.edu','Kim','','Scott',0,0,1,0),
 	(default,'jfrits00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','janet.fritsche@uafs.edu','Janet','G','Fritsche',0,0,1,0),
 	(default,'tscurl00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','terry.scurlock@uafs.edu','Terry','S','Scurlock',0,0,1,0),
 	(default,'smered00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','sandra.meredith@uafs.edu','Sandra','S','Meredith',0,0,1,0),
@@ -72,7 +72,7 @@ VALUES
 	/*Administrator  59*/
 	(default,'kbuckw00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','karen.admin@uafs.edu','Karen','J','Buckwater',0,0,0,1),
 	(default,'msmith00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','mary.admin@uafs.edu','Mary','','Smtih',0,0,0,1),
-    	(default,'gjames00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','gray.admin@uafs.edu','Gray','M','James',0,0,0,1);
+        (default,'gjames00','$2y$10$fmncGYw52EWo2cqxN/ryaeg2tNtvoVGs1C90M.H3DBLjEnhM1bxPy','gray.admin@uafs.edu','Gray','M','James',0,0,0,1);
     
 INSERT INTO college
 VALUES
@@ -227,6 +227,95 @@ VALUES
 	(default,24,TRUE,1054,1030,2),
 	(default,25,FALSE,0578,NULL,9),
 	(default,26,FALSE,3063,NULL,9);
+	
+INSERT INTO building
+VALUES
+	(default,'Baldor Technology Center','BD'),
+	(default,'Ballman-Speer Building','BS'),
+	(default,'Breedlove Building','BB'),
+	(default,'Echols Building','EC'),
+	(default,'Business Center','BC'),
+	(default,'Flanders Business Center','FL'),
+	(default,'Pendergraft Health Sciences Center','HS'),
+	(default,'Holt Building','HT'),
+	(default,'Math-Science Building','MS'),
+	(default,'Windgate Art and Design Building','WB');
+	
+INSERT INTO room
+VALUES 
+	(default,'143',1,20),
+	(default,'142',1,20),
+	(default,'201',1,15),
+	(default,'101',2,30),
+	(default,'203',2,15),
+	(default,'112',2,25),
+	(default,'101A',3,25),
+	(default,'101B',3,25),
+	(default,'102',3,15),
+	(default,'201',4,20),
+	(default,'202',4,20),
+	(default,'101',4,15),
+	(default,'105',5,12),
+	(default,'103',5,20),
+	(default,'107',5,30),
+	(default,'102A',6,25),
+	(default,'102B',6,20),
+	(default,'101',6,15),
+	(default,'201',7,20),
+	(default,'202',7,20),
+	(default,'101',7,25),
+	(default,'101',8,25),
+	(default,'202',8,25),
+	(default,'203',8,15),
+	(default,'103',9,25),
+	(default,'102',9,20),
+	(default,'101',9,10),
+	(default,'130',10,20),
+	(default,'132',10,25),
+	(default,'101',10,20);
+	
+INSERT INTO subject 
+VALUES
+	(default,'Accounting (ACCT)'),
+	(default,'Animation Technology (BSAT)'),
+	(default,'Art (ART)'),
+	(default,'Biology (BIOL)'),
+	(default,'Chemistry (CHEM)'),
+	(default,'Computer & Information Science (CS)'),
+	(default,'Dental Hygiene (DHYG)'),
+	(default,'Education (EDUC)'),
+	(default,'Engineering (ENGN)'),
+	(default,'Finance (FIN)'),
+	(default,'History (HIST)'),
+	(default,'Humanities (HUMN)'),
+	(default,'Marketing (MKTG)'),
+	(default,'Mathematics (MATH)'),
+	(default,'Welding Technology (WELD)'),
+	(default,'Speech (SPCH)');
+	
+INSERT INTO course
+VALUES
+	(default,'Principles of Financial Accounting','Introduces financial accounting theory, including the accounting cycle, analysis and recording of transactions, and reporting financial information in accordance with Generally Accepted Accounting Principles (GAAP).',3,1),
+	(default,'Introduction to Animation','Students will have a chance to focus their study in a particular area including 3D character animation, 2D character animation, modeling, and character design.',3,2),
+	(default,'2D Design','Through direct experience with the art making process, this course will help you solve visual problems, improve your ability to discuss artwork, and allow you to gain a solid understanding of the elements and principles of design. ',3,3),
+	(default,'Digital Imaging','This course focuses on the continued use of digital imagery in art and design, by furthering a students skill in the use of raster image (Adobe Photoshop), and scanning software.',3,3),
+	(default,'Biological Science','A course emphasizing scientific inquiry through selected concepts of biology including: chemical and cellular organization, function, genetics, evolution and ecology.',3,4),
+	(default,'Biological Science Lab','Lab course for Biological Science',1,4),
+	(default,'College Chemistry I','Introduction to the general principles of chemistry for students planning a professional career in chemistry, a related science, the health professions, or engineering.',3,5),
+	(default,'College Chemistry I Lab','Lab course for College Chemistry I',1,5),
+	(default,'Foundations of Programming I','Topics include software development methodology, data types, control structures, functions, arrays, and the mechanics of running, testing, and debugging.',3,6),
+	(default,'Web Systems','This class leads students through the beginning Web Site creation process while developing and enhancing HTML, CSS, and visual design skills along the way. Students will begin by learning about layout and navigation, progress to Web typography, colors and images, working with CSS and more.',3,6),
+	(default,'Computer Architecture','An overview of computer architecture, which stresses the underlying design principles and the impact of these principles on computer performance. General topics include design methodology, processor design, control design, memory organization, system organization, and parallel processing.',3,6),
+	(default,'Dental Anatomy','The lecture portion of the course introduces the student to basic terminology for understanding the structures that form the foundation for tooth function, normal anatomy of the oral cavity, and tooth and root morphology.',3,7),
+	(default,'Intro to Education','Introduction to the historical, cultural, and philosophical foundations of our system of American public education. ',3,8),
+	(default,'Internship - ELEM','Internship at local elementary school. Must be approved. ',3,8),
+	(default,'Intro to Engineering I','This course explores the branches of engineering, the functions of an engineer, and the industries in which engineers work.',3,9),
+	(default,'Engineering Statistics','This course covers the role of statistics in engineering, probability, discrete random variables and probability distributions, continuous random variables and probability distributions, joint probability distributions, random sampling and data description',3,9);
+
+	
+
+
+	
     
 	
 	
